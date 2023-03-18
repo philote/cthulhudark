763c93078212137517d71356c6b4b15f61db7252
Joseph Hopson <727468+philote@users.noreply.github.com>
2023-03-17 18:56:44 -0600

    moved the handlebar helpers out of the init hook

--- module/cthulhudark.mjs
+++ module/cthulhudark.mjs
@@ -32,73 +32,72 @@ Hooks.once('init', async function() {
   Actors.registerSheet("cthulhudark", CthulhuDarkActorSheet, { makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("cthulhudark", CthulhuDarkItemSheet, { makeDefault: true });
-
-  /* -------------------------------------------- */
-  /*  Handlebars Helpers                          */
-  /* -------------------------------------------- */
-
-  // Multiboxes from Blades in the Dark system module. 
-  // https://github.com/megastruktur/foundryvtt-blades-in-the-dark
-  Handlebars.registerHelper('multiboxes', function(selected, options) {
-
-    let html = options.fn(this);
-
-    // Fix for single non-array values.
-    if ( !Array.isArray(selected) ) {
-      selected = [selected];
-    }
-
-    if (typeof selected !== 'undefined') {
-      selected.forEach(selected_value => {
-        if (selected_value !== false) {
-          let escapedValue = RegExp.escape(Handlebars.escapeExpression(selected_value));
-          let rgx = new RegExp(' value=\"' + escapedValue + '\"');
-          let oldHtml = html;
-          html = html.replace(rgx, "$& checked");
-          while( ( oldHtml === html ) && ( escapedValue >= 0 ) ){
-            escapedValue--;
-            rgx = new RegExp(' value=\"' + escapedValue + '\"');
-            html = html.replace(rgx, "$& checked");
-          }
-        }
-      });
-    }
-    return html;
-  });
-
-  // "N Times" loop for handlebars.
-  //  Block is executed N times starting from n=1.
-  //
-  // Usage:
-  // {{#times_from_1 10}}
-  //   <span>{{this}}</span>
-  // {{/times_from_1}}
-  Handlebars.registerHelper('times_from_1', function(n, block) {
-
-    var accum = '';
-    for (var i = 1; i <= n; ++i) {
-      accum += block.fn(i);
-    }
-    return accum;
-  });
-
-  // "N Times" loop for handlebars.
-  //  Block is executed N times starting from n=0.
-  //
-  // Usage:
-  // {{#times_from_0 10}}
-  //   <span>{{this}}</span>
-  // {{/times_from_0}}
-  Handlebars.registerHelper('times_from_0', function(n, block) {
-
-    var accum = '';
-    for (var i = 0; i <= n; ++i) {
-      accum += block.fn(i);
-    }
-    return accum;
-  });
 });
 
+/* -------------------------------------------- */
+/*  Handlebars Helpers                          */
+/* -------------------------------------------- */
+
+// Multiboxes from Blades in the Dark system module. 
+// https://github.com/megastruktur/foundryvtt-blades-in-the-dark
+Handlebars.registerHelper('multiboxes', function(selected, options) {
+
+  let html = options.fn(this);
+
+  // Fix for single non-array values.
+  if ( !Array.isArray(selected) ) {
+    selected = [selected];
+  }
+
+  if (typeof selected !== 'undefined') {
+    selected.forEach(selected_value => {
+      if (selected_value !== false) {
+        let escapedValue = RegExp.escape(Handlebars.escapeExpression(selected_value));
+        let rgx = new RegExp(' value=\"' + escapedValue + '\"');
+        let oldHtml = html;
+        html = html.replace(rgx, "$& checked");
+        while( ( oldHtml === html ) && ( escapedValue >= 0 ) ){
+          escapedValue--;
+          rgx = new RegExp(' value=\"' + escapedValue + '\"');
+          html = html.replace(rgx, "$& checked");
+        }
+      }
+    });
+  }
+  return html;
+});
+
+// "N Times" loop for handlebars.
+//  Block is executed N times starting from n=1.
+//
+// Usage:
+// {{#times_from_1 10}}
+//   <span>{{this}}</span>
+// {{/times_from_1}}
+Handlebars.registerHelper('times_from_1', function(n, block) {
+
+  var accum = '';
+  for (var i = 1; i <= n; ++i) {
+    accum += block.fn(i);
+  }
+  return accum;
+});
+
+// "N Times" loop for handlebars.
+//  Block is executed N times starting from n=0.
+//
+// Usage:
+// {{#times_from_0 10}}
+//   <span>{{this}}</span>
+// {{/times_from_0}}
+Handlebars.registerHelper('times_from_0', function(n, block) {
+
+  var accum = '';
+  for (var i = 0; i <= n; ++i) {
+    accum += block.fn(i);
+  }
+  return accum;
+});
 
 // // If you need to add Handlebars helpers, here are a few useful examples:
 // Handlebars.registerHelper('concat', function() {
